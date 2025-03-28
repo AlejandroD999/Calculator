dc804f82846887d6b85897f3aeecd1a657870242
Alejandro D. <alejandrodgonzalez923@gmail.com>
2025-03-27 20:57:12 -0500

    Added Class on 'home.py' and basic layout for calculator GUI

--- home.py
+++ home.py
@@ -1,1 +1,20 @@
-...
+import tkinter as tk
+from tkinter import messagebox
+
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('Calculator')
+        self.geometry('300x400')
+    
+
+
+
+
+if __name__ == '__main__':
+    app = App()
+
+    app.mainloop()
