e44dd61a71120f8ad818e310c7544b76a1a3c79e
Alejandro D. <alejandrodgonzalez923@gmail.com>
2025-03-27 21:15:45 -0500

    Added widget and small frame| Note: Finish making the square the same width as window App

--- home.py
+++ home.py
@@ -7,14 +7,23 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        self.window_height = self.winfo_height()
+        print(self.window_height)
         self.title('Calculator')
         self.geometry('300x400')
-    
+
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        self.topframe = tk.Frame(self, bg='gray',width=5, height=50)
+
+        self.topframe.pack()
 
 
-
-
+ 
 if __name__ == '__main__':
     app = App()
 
     app.mainloop()
+
